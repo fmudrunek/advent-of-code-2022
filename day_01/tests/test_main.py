ebf7f1b248685b86554b11742c0452e0a74528f2
Filip Mudruenk <filip.mudrunek@gmail.com>
2022-12-06 12:33:08 +1100

    Test data path fixed

--- day_01/tests/test_main.py
+++ day_01/tests/test_main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from ..main import solve_part_1, solve_part_2
 
 def test_solve_part_1():
-    input_path = Path("tests/test_data_1.txt")
+    input_path = Path(__file__).parent / "test_data_1.txt"
     expected = 24000
 
     actual = solve_part_1(input_path)
@@ -10,7 +10,7 @@ def test_solve_part_1():
     assert actual == expected 
 
 def test_solve_part_2():
-    input_path = Path("tests/test_data_1.txt")
+    input_path = Path(__file__).parent / "test_data_1.txt"
     expected = 45000
 
     actual = solve_part_2(input_path)
