271e312d1603af71734758692b718280294e89c4
Filip Mudruenk <filip.mudrunek@gmail.com>
2022-12-06 16:37:20 +1100

    Whitespace cleanup

--- day_02/tests/test_main.py
+++ day_02/tests/test_main.py
@@ -5,7 +5,7 @@ test_data = get_data(Path(__file__).parent / "test_data_1.txt")
 
 def test_solve_part_1():
     actual = solve_part_1(test_data)
-    assert actual == 15 
+    assert actual == 15
 
 def test_solve_part_2():
     actual = solve_part_2(test_data)
