6548b18ea87590649a77af2b4b236edbe5d5327a
Filip Mudruenk <filip.mudrunek@gmail.com>
2022-12-06 16:28:48 +1100

    Cleaning up day 3 solution

--- day_03/main.py
+++ day_03/main.py
@@ -1,43 +1,41 @@
 from pathlib import Path
-
+from typing import List
+from more_itertools import chunked
 
 def _char_to_int(letter: str) -> int:
     if (letter.isupper()):
         return ord(letter) - 38
     return ord(letter) - 96
 
-def solve_part_1(input_path: Path) ->  int:
+def solve_part_1(data: List[str]) ->  int:
     result = 0
-    with input_path.open() as input_file:
-        for line in input_file:
-            stripped = line.strip()
-            compartment1, compartment2 = stripped[:len(stripped)//2], stripped[len(stripped)//2:]
+    for line in data:
+        compartment1, compartment2 = line[:len(line)//2], line[len(line)//2:]
 
-            intersection = set(compartment1).intersection(set(compartment2)).pop()
-            result += _char_to_int(intersection)
+        intersection = set(compartment1).intersection(set(compartment2)).pop()
+        result += _char_to_int(intersection)
 
     return result
 
 
-def solve_part_2(input_path: Path) -> int:
+def solve_part_2(data: List[str]) -> int:
     result = 0
-    with input_path.open() as input_file:
-        for line in input_file:
-            stripped = line.strip()
-            line2, line3 = next(input_file).strip(), next(input_file).strip()
-
-            intersection = set(stripped).intersection(set(line2), set(line3)).pop()
-            result += _char_to_int(intersection)
+    for chunked_lines in chunked(data, 3):
+        intersection = set(chunked_lines[0]).intersection(set(chunked_lines[1]), set(chunked_lines[2])).pop()
+        result += _char_to_int(intersection)
 
     return result
 
 
 
-
+def get_data(path: Path) -> List[str]:
+    with path.open() as input_file:
+        return [line.strip() for line in input_file.readlines()]
 
 if __name__ == "__main__":
     input_path = Path(__file__).parent / "input_data.txt"
-    result_part_1 = solve_part_1(input_path)
-    result_part_2 = solve_part_2(input_path)
+    data = get_data(input_path)
+    result_part_1 = solve_part_1(data)
+    result_part_2 = solve_part_2(data)
     print(f"Part 1 result: {result_part_1}\nPart 2 result: {result_part_2}")
 
--- day_03/tests/test_main.py
+++ day_03/tests/test_main.py
@@ -1,18 +1,12 @@
 from pathlib import Path
-from ..main import solve_part_1, solve_part_2
+from ..main import solve_part_1, solve_part_2, get_data
+
+test_data = get_data(Path(__file__).parent / "test_data_1.txt")
 
 def test_solve_part_1():
-    input_path = input_path = Path(__file__).parent / "test_data_1.txt"
-    expected = 157
-
-    actual = solve_part_1(input_path)
-    
-    assert actual == expected 
+    actual = solve_part_1(test_data)
+    assert actual == 157 
 
 def test_solve_part_2():
-    input_path = input_path = Path(__file__).parent / "test_data_1.txt"
-    expected = 70
-
-    actual = solve_part_2(input_path)
-    
-    assert actual == expected 
+    actual = solve_part_2(test_data)
+    assert actual == 70
